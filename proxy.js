0ce9de9acf75fd3ea3edafdfc0312fa93b7d0994
Luc Charest (qub.ca) <code@qub.ca>
2017-06-23 12:54:34 -0400

    Add cache control by host in config, module refactoring

--- proxy.js
+++ proxy.js
@@ -1,43 +1,35 @@
-﻿var http = require('http');
+﻿//var proxy = module.exports;
+var http = require('http');
 var https = require('https');
 var fs = require('fs');
-/* var mime = require('mime'); */
-var config = {};
-var $cache = {};
-module.exports = {
+$ = {
+    config: {},
+    cache: {},
     init: function () {
         // Load Config
         if (fs.existsSync("./config.json")) {
             var configFile = fs.readFileSync("./config.json");
-            config = JSON.parse(configFile);
+            $.config = JSON.parse(configFile);
         }
-        if (!fs.existsSync(config.cachePath)) {
-            fs.mkdir(config.cachePath, function () { Log("Cache folder created"); });
+        if (!fs.existsSync($.config.cachePath)) {
+            fs.mkdir($.config.cachePath, function () { Log("Cache folder created"); });
         }
+
         // Load cache
-        for (var host in config.hostList) {
-            if (!fs.existsSync(config.cachePath + host + "/")) {
-                fs.mkdir(config.cachePath + host + "/");
+        for (var host in $.config.hostList) {
+            if (!fs.existsSync($.config.cachePath + host + "/")) {
+                fs.mkdir($.config.cachePath + host + "/");
             }
-            $cache[host] = {};
-            if (fs.existsSync(config.cachePath + host + "/cache.json")) {
-                var cacheFile = fs.readFileSync(config.cachePath + host + "/cache.json");
-                $cache[host] = JSON.parse(cacheFile);
+            $.cache[host] = {};
+            if (fs.existsSync($.config.cachePath + host + "/cache.json")) {
+                var cacheFile = fs.readFileSync($.config.cachePath + host + "/cache.json");
+                $.cache[host] = JSON.parse(cacheFile);
             }
         }
         setInterval(this.tick, 10000);
     },
     tick: function () {
-        for (var host in config.hostList) {
-            if (cacheChangedList[host]) {
-                if (new Date().getTime() > cacheChangedList[host]) {
-                    delete cacheChangedList[host];
-                    fs.writeFile(config.cachePath + host + "/cache.json", JSON.stringify($cache[host]), 'utf-8', function (err) { }); 
-                    Log("CACHE SAVED: " + host);
-                }
-            }
-
-        } 
+        $.cacheTools.eventChangedExecute();
     },
     proceed: function (req, res) {
         try {
@@ -47,7 +39,7 @@ module.exports = {
             var pCacheKey = pURL;
             res.statusCode = 200;
             
-            if (!config.hostList[pHost]) {
+            if (!$.config.hostList[pHost]) {
                 res.end("NOT AUTHORIZED PROXY ACCESS");
                 return false;
             }
@@ -58,14 +50,14 @@ module.exports = {
             });
             req.on('end', function () {
                 if (body !== "") { pCacheKey += "|" + body; }
-                if ($cache[pHost][pCacheKey]) {
-                    res.writeHeader($cache[pHost][pCacheKey].statusCode, $cache[pHost][pCacheKey].headers);
-                    if ($cache[pHost][pCacheKey].content !== "") {
-                        res.end($cache[pHost][pCacheKey].content);
+                if ($.cache[pHost][pCacheKey] && $.config.hostList[pHost].cacheEnabled) {
+                    res.writeHeader($.cache[pHost][pCacheKey].statusCode, $.cache[pHost][pCacheKey].headers);
+                    if ($.cache[pHost][pCacheKey].content !== "") {
+                        res.end($.cache[pHost][pCacheKey].content);
                         Log("CACHE TEXT: " + pURL);
                     } else {
                         try {
-                            var readStream = fs.createReadStream($cache[pHost][pCacheKey].filePath);
+                            var readStream = fs.createReadStream($.cache[pHost][pCacheKey].filePath);
                             readStream.pipe(res);
                         } catch (err) {
                             res.end();
@@ -77,7 +69,7 @@ module.exports = {
                 } else {
                     var pOptions = {
                         method: req.method,
-                        hostname: config.hostList[pHost].ip,
+                        hostname: $.config.hostList[pHost].ip,
                         port: 80,
                         path: pURL,
                         headers: req.headers
@@ -86,35 +78,38 @@ module.exports = {
                     delete pOptions.headers["accept-encoding"]; // Disable GZip
                     pOptions.headers["host"] = pHost;
 
-                    var pCacheFilePath = cacheGetPath(pHost, pURL);
+                    var pCacheFilePath = $.cacheTools.getPath(pHost, pURL);
 
                     // Send Request to origin
                     var extReq = http.request(pOptions, function (extRes) {
                         res.writeHeader(extRes.statusCode, extRes.headers);
-                        var pContentType = "";
-                        if (extRes.headers["content-type"]) pContentType = extRes.headers["content-type"];
 
-                        $cache[pHost][pCacheKey] = {
-                            statusCode: extRes.statusCode,
-                            headers: extRes.headers,
-                            content: "",
-                            filePath: pCacheFilePath
-                        };
-                        //var pContentTypeText = "|text/html|text/css|";
-                        if (pContentType.indexOf("text") !== -1 || pContentType.indexOf("javascript") !== -1 || pContentType.indexOf("json") !== -1) {
-                            var resBody = "";
-                            extRes.on('data', function (chunk) {
-                                resBody += chunk;
-                            });
-                            extRes.on('end', function (chunk) {
-                                $cache[pHost][pCacheKey].content = resBody;
-                            });
+                        if ($.config.hostList[pHost].cacheEnabled) {
+                            var pContentType = "";
+                            if (extRes.headers["content-type"]) pContentType = extRes.headers["content-type"];
 
-                        } else {
-                            cacheCreatePath(pCacheFilePath);
-                            extRes.pipe(fs.createWriteStream(pCacheFilePath));
+                            $.cache[pHost][pCacheKey] = {
+                                statusCode: extRes.statusCode,
+                                headers: extRes.headers,
+                                content: "",
+                                filePath: pCacheFilePath
+                            };
+                        
+                            if (pContentType.indexOf("text") !== -1 || pContentType.indexOf("javascript") !== -1 || pContentType.indexOf("json") !== -1) {
+                                var resBody = "";
+                                extRes.on('data', function (chunk) {
+                                    resBody += chunk;
+                                });
+                                extRes.on('end', function (chunk) {
+                                    $.cache[pHost][pCacheKey].content = resBody;
+                                });
+
+                            } else {
+                                $.cacheTools.createPath(pCacheFilePath);
+                                extRes.pipe(fs.createWriteStream(pCacheFilePath));
+                            }
+                            $.cacheTools.eventChanged(pHost);
                         }
-                        cacheChanged(pHost);
                         
                         extRes.pipe(res);
                         Log("RELAY: " + pURL);
@@ -131,50 +126,63 @@ module.exports = {
         }
 
     },
-    getConfig: function () { return config; },
-    getCache: function () { return $cache; }
+    cacheTools: {
+        createPath: function (path) {
+            //Log("CREATING: " + path);
+            var pFolders = path.split("/");
+            var pPathCreate = "";
+            for (var i = 0; i < pFolders.length - 1; i++) {
+                pPathCreate += pFolders[i] + "/";
+                if (!fs.existsSync(pPathCreate)) {
+                    fs.mkdirSync(pPathCreate);
+                    //Log("CREATE: " + pPathCreate);
+                }
+
+            }
+        },
+        getPath: function (host, url) {
+            if (url.indexOf("?") !== -1) {
+                /*var arrayURL = url.split("/");
+                if (arrayURL[arrayURL.length - 1].indexOf(".") === -1) { return "DONT-CACHE"; }*/
+                url = url.split("?")[0];
+                //return "DONT-CACHE";
+            }
+            /*if (url.indexOf(".aspx") !== -1) { return "DONT-CACHE"; }*/
+            var pPath = $.config.cachePath + host;
+            var pFolders = url.split("/");
+            for (var i = 0; i < pFolders.length - 1; i++) {
+                pPath += pFolders[i] + "/";
+                //Log(pPath);
+            }
+            var pFileName = pFolders[pFolders.length - 1];
+            if (pFileName === "") { pFileName = "index.html"; } /* http://domain.com/test/ */
+            if (pFileName.indexOf(".") === -1) { pFileName += "/index.html"; } /* http://domain.com/test */
+            pPath += pFileName;
+            return pPath;
+        },
+        eventChangedList: {},
+        eventChanged: function (host) {
+            $.cacheTools.eventChangedList[host] = new Date().getTime() + 10000;
+        },
+        eventChangedExecute: function () {
+            for (var host in $.config.hostList) {
+                if ($.cacheTools.eventChangedList[host]) {
+                    if (new Date().getTime() > $.cacheTools.eventChangedList[host]) {
+                        delete $.cacheTools.eventChangedList[host];
+                        fs.writeFile($.config.cachePath + host + "/cache.json", JSON.stringify($.cache[host]), 'utf-8', function (err) { });
+                        Log("CACHE SAVED: " + host);
+                    }
+                }
+
+            } 
+        }
+    },
+    getConfig: function () { return $.config; },
+    getCache: function () { return $.cache; }
  
 };
+module.exports = $;
 
-var cacheChangedList = {};
-var cacheChanged = function (host) {
-    cacheChangedList[host] = new Date().getTime() + 10000; 
-};
-
-var cacheCreatePath = function (path) {
-    //Log("CREATING: " + path);
-    var pFolders = path.split("/");
-    var pPathCreate = "";
-    for (var i = 0; i < pFolders.length - 1; i++) {
-        pPathCreate += pFolders[i] + "/";
-        if (!fs.existsSync(pPathCreate)) {
-            fs.mkdirSync(pPathCreate);
-            //Log("CREATE: " + pPathCreate);
-        }
-
-    }
-};
-
-var cacheGetPath = function (host, url) {
-    if (url.indexOf("?") !== -1) {
-        /*var arrayURL = url.split("/");
-        if (arrayURL[arrayURL.length - 1].indexOf(".") === -1) { return "DONT-CACHE"; }*/
-        url = url.split("?")[0];
-        //return "DONT-CACHE";
-    }
-    /*if (url.indexOf(".aspx") !== -1) { return "DONT-CACHE"; }*/
-    var pPath = config.cachePath + host;
-    var pFolders = url.split("/");
-    for (var i = 0; i < pFolders.length - 1; i++) {
-        pPath += pFolders[i] + "/";
-        //Log(pPath);
-    }
-    var pFileName = pFolders[pFolders.length - 1];
-    if (pFileName === "") { pFileName = "index.html"; } /* http://domain.com/test/ */
-    if (pFileName.indexOf(".") === -1) { pFileName += "/index.html"; } /* http://domain.com/test */
-    pPath += pFileName;
-    return pPath;
-};
 
 String.prototype.replaceAll = function (search, replacement) {
     var target = this;
--- routes.js
+++ routes.js
@@ -7,6 +7,7 @@ module.exports = {
     execute: function (req, res) {
         switch (req.url) {
             case "/cache/":
+                /* CACHE MODULE */
                 res.setHeader("content-type", "text/html; charset=UTF-8");
                 //res.setHeader("content-type", "application/json");
                 //res.end(JSON.stringify(proxy.getConfig()));
@@ -17,10 +18,21 @@ module.exports = {
                     H += "<h1>" + host + "</h1>";
                     for (var url in cache[host]) {
                         H += "<h2>" + url + "</h2>";
+                        H += cache[host][url].filePath + "<br>";
                         H += JSON.stringify(cache[host][url].headers);
+                        if (cache[host][url].headers["content-type"]) {
+                            if (cache[host][url].headers["content-type"].indexOf("image") !== -1) {
+                                H += "<img src=\"" + url + "\" width='100' /><br>";
+                            }
+                        }
+                        if (cache[host][url].content !== "") {
+                            //H += "<textarea>" + cache[host][url].content + "</textarea>";
+                        }
                     }
                 //}
                 res.end(H);
+
+                /* CACHE MODULE */
                 break;
 
             default:
